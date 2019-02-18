dee5a2fafcf17152140533fcbbc1652e7dca2970
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-18 16:43:17 +0000

    add form

--- app.rb
+++ app.rb
@@ -15,7 +15,13 @@ get '/third' do
   "deepest darkest secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
