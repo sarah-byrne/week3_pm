cbc51879811ccfc96870d88c260125f18c0c1ffa
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-18 15:11:12 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+gem 'sinatra', '~> 2.0', '>= 2.0.5'
+gem 'shotgun', '~> 0.9.2'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'shotgun
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello world"
+end
+
+get '/secret' do
+  "deepest darkest secret!"
+end
+
+get '/third' do
+  "deepest darkest secret!"
+end
