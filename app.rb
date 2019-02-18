3bd26f4281199818fec33d3f10b0ba6226bae242
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-18 17:05:32 +0000

    post form

--- app.rb
+++ app.rb
@@ -20,8 +20,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:name_form)
 end
